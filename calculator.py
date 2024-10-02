21447ec497886b92962887a6ac818fd397e6dd2d
Decse1 <paller.peter.decse@diak.szbi-pg.hu>
2024-10-02 11:22:06 +0200

    Add calculator file

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def calculate_sum(a,b):
+    return a+b;
+
+def calculate_substraction(a,b):
+    return a-b;
+
+def calculate_multiply(a,b):
+    return a*b;
+
+def calculate_divide(a,b):
+    return a-b;
+
+a = int(input("Adja meg az elso szamot: "));
+mj = string(input("Adja meg a muveleti jelet[+ - * /]: "))
+b = int(input("Adja meg a masodik szamot: "));
+
+
