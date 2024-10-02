8925ce3d216b3d9aac56efcb63abd05e56055d41
Decse1 <paller.peter.decse@diak.szbi-pg.hu>
2024-10-02 12:11:00 +0200

    Update calculator file

--- calculator.py
+++ calculator.py
@@ -1,17 +1,31 @@
+import os
 def calculate_sum(a,b):
     return a+b;
 
-def calculate_substraction(a,b):
+def calculate_sub(a,b):
     return a-b;
 
-def calculate_multiply(a,b):
+def calculate_mult(a,b):
     return a*b;
 
 def calculate_divide(a,b):
     return a-b;
 
 a = int(input("Adja meg az elso szamot: "));
-mj = string(input("Adja meg a muveleti jelet[+ - * /]: "))
+mj = string("");
+while(mj != "+" or mj != "-" or mj != "*" or mj != "/"):
+    os.system('cls');
+    mj = string(input("Adja meg a muveleti jelet[+ - * /]: "));
 b = int(input("Adja meg a masodik szamot: "));
+
+if(mj = "+"):
+    os.system('cls');
+    calculate_sum(a,b);
+if(mj = "-"):
+    os.system('cls');
+    calculate_sub(a,b);
+if(mj = "-"):
+    os.system('cls');
+    calculate_sub(a,b);
 
 
